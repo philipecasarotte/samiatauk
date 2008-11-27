d721895ed7ec6bfc5557f78975521c053f3a854e
Marcio Trindade <marciotrindade@gmail.com>
2008-11-27 00:27:12 -0200

    changed the username of default user

--- db/migrate/20081126212258_create_admin_role.rb
+++ db/migrate/20081126212258_create_admin_role.rb
@@ -1,12 +1,12 @@
 class CreateAdminRole < ActiveRecord::Migration
   def self.up
-    u = User.create(:login => "admin", :password => "bundinha", :password_confirmation => "bundinha", :email => "dev.dburns@gmail.com")
+    u = User.create(:login => "danieltburns", :password => "bundinha", :password_confirmation => "bundinha", :email => "dev.dburns@gmail.com")
     r = Role.create(:name => "admin")
     u.roles << r
   end
 
   def self.down
     Role.find_by_name("admin").destroy
-    User.find_by_login("admin").destroy
+    User.find_by_login("danieltburns").destroy
   end
 end
