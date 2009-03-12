751fb2bc9fbfddb0114357e83a0956a8e2e76bff
Ricardo Shiota Yasuda <shadow11@gmail.com>
2009-03-12 10:42:36 -0300

    Removed consider all requests local from development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,5 +16,3 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :test
-
-config.action_controller.consider_all_requests_local = false
