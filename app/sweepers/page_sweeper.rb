faa746de873f4606fb2f003f752f5984fad04d9e
Marcio Trindade <marciotrindade@gmail.com>
2009-04-16 22:53:01 -0300

    re-factoring pages

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -2,10 +2,14 @@ class PageSweeper < ActionController::Caching::Sweeper
   observe Page
 
   def after_save(page)
-    ["/pages/#{page.permalink}", '/index'].each{|action| expire_page(action) }
+    clear_page_cache(page)
   end
 
   def after_destroy(page)
+    clear_page_cache(page)
+  end
+
+  def clear_page_cache(page)
     ["/pages/#{page.permalink}", '/index'].each{|action| expire_page(action) }
   end
 
