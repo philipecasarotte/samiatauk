3b8ea96e28b83bfa11bd6e1d950de4d8719f3790
Ricardo Shiota Yasuda <shadow11@gmail.com>
2009-05-19 19:56:55 -0300

    Integration test for admin authentication

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -38,4 +38,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  Webrat.configure do |config|
+    config.mode = :rails
+  end
+  
 end
