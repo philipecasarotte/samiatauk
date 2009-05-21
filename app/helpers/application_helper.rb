538186709a31cc44cf3a5cd4493ba6665ef35edf
Marcio Trindade <marciotrindade@gmail.com>
2009-05-21 11:48:04 -0300

    add helper to javascript and stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
 
-  def validate_i18n
-    "plugins/validate_" + I18n.locale
-    rescue
-      "plugins/validate_en"
-  end
+	def validate_i18n
+		"plugins/validate_" + I18n.locale
+		rescue
+			"plugins/validate_en"
+	end
 
+	def javascript(*files)
+		content_for(:head) { javascript_include_tag(*files) }
+	end
+
+	def stylesheet(*files)
+		content_for(:head) { stylesheet_link_tag(*files) }
+	end
 end
