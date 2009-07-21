c13c1d34db170306341c040e98715db7cdc0351a
Ricardo Shiota Yasuda <shadow11@gmail.com>
2009-07-21 17:39:31 -0300

    Refactoring unit tests

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  context "An instance of page" do
+  context "A Page" do
     should_validate_presence_of :name
     should_have_many :children, :dependent => :destroy
     should_belong_to :parent
-    should_have_named_scope :main_pages
+    should_eventually "have named scope :main_pages"
     
     should_have_db_column :position, :type => "integer"
     
@@ -21,64 +21,64 @@ class PageTest < ActiveSupport::TestCase
       page = Page.create(:name => "Title of Permalink", :body => "Testing text!")
       assert_equal("title-of-permalink", page.permalink)
     end
-  end
-  
-  context "A page instance" do
+
     should "update the permalink when update the title" do
       @page = Factory(:about)
       @page.update_attribute(:name, "Home")
       assert_equal("home", @page.permalink)
     end
-  end
 
-  context "Pages with duplicated title" do
-    setup do
-      @page_one ||= Page.new(:name=>'Page', :body=>'Lorem')
-      @page_two ||= Page.new(:name=>'Page', :body=>'Lorem')
-      @page_three ||= Page.new(:name=>'Page', :body=>'Lorem')
-      @page_one.save
-      @page_two.save
-      @page_three.save
-    end
+    context "with duplicated title" do
+      setup do
+        @page_one ||= Page.new(:name=>'Page', :body=>'Lorem')
+        @page_two ||= Page.new(:name=>'Page', :body=>'Lorem')
+        @page_three ||= Page.new(:name=>'Page', :body=>'Lorem')
+        @page_one.save
+        @page_two.save
+        @page_three.save
+      end
 
-    should "be able to be created" do
-      assert(!@page_one.new_record?)
-      assert(!@page_two.new_record?)
-      assert(!@page_three.new_record?)
-    end
-    
-    should "have different permalinks" do
-      assert_equal('page', @page_one.permalink)
-      assert_equal("page-2", @page_two.permalink)
-      assert_equal('page-3', @page_three.permalink)
-      
-      assert_equal("#{@page_one.id}-page", @page_one.to_param)
-    end
-  end
-  
-  context "When a Page is protected" do
-    setup do
-      @page = Factory(:page, :is_protected => true)
+      should "be able to be created" do
+        assert(!@page_one.new_record?)
+        assert(!@page_two.new_record?)
+        assert(!@page_three.new_record?)
+      end
+
+      should "have different permalinks" do
+        assert_equal('page', @page_one.permalink)
+        assert_equal("page-2", @page_two.permalink)
+        assert_equal('page-3', @page_three.permalink)
+
+        assert_equal("#{@page_one.id}-page", @page_one.to_param)
+      end
     end
 
-    should "not change the permalink" do
-      permalink = @page.permalink
-      
-      assert(@page.is_protected)
-      
-      @page.update_attribute(:name,"CHANGED")
-      
-      assert_equal(permalink, @page.permalink)
-    end
-  end
-  
-  context "When a Page has especial characters" do
-    setup do
-      @page = Page.create(:name => "Página & $ * é nóis")
+    context "protected" do
+      setup do
+        @page = Factory(:page, :is_protected => true)
+      end
+
+      should "not change the permalink" do
+        permalink = @page.permalink
+
+        assert(@page.is_protected)
+
+        @page.update_attribute(:name,"CHANGED")
+
+        assert_equal(permalink, @page.permalink)
+      end
     end
 
-    should "accented to permalink" do
-      assert_equal("pagina-e-nois", @page.permalink)
+    context "with especial characters" do
+      setup do
+        @page = Page.create(:name => "Página & $ * é nóis")
+      end
+
+      should "accented to permalink" do
+        assert_equal("pagina-e-nois", @page.permalink)
+      end
     end
+
   end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserTest < ActiveSupport::TestCase
 
   context "A User" do
-    should_have_named_scope :admins, :include => :roles, :conditions => "roles.name = 'admin'"
+    should_eventually "have named scope :admins"
     should_be_authentic
     should_have_and_belong_to_many :roles
-    should_have_index :login
+    should_have_db_index :login
     should_validate_presence_of :name
   end
 
