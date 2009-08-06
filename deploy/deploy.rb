13679241359918202770135f9d28dd7f24f4b3ed
Marcio Trindade <marciotrindade@gmail.com>
2009-08-06 00:39:06 -0300

    add the new deploy methods (like github)

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -8,6 +8,7 @@ set :domain, "domain.com"
 set :application, "app_name"
 
 set :repository, "git://dburnsdesign.com/repos/mainline.git"
+set :branch,     "origin/master"
 # =============================================================================
 # NAO MEXER DAQUI PARA BAIXO
 # =============================================================================
@@ -16,54 +17,71 @@ role :app, host
 role :db,  host
 
 set :deploy_to, "/home/#{user}" 
-set :public_html, "/home/#{user}/public_html"
-set :current_deployment, "#{deploy_to}/current"
-
-set :runner, nil
-set :use_sudo, false
 
 set :scm, :git
 
-set :keep_releases, 3
+set :current_release, 
 
 ssh_options[:username] = user
 ssh_options[:paranoid] = false
 # =============================================================================
 # TAREFAS - NAO MEXER A MENOS QUE SAIBA O QUE ESTA FAZENDO
 # =============================================================================
-desc "Garante que o database.yml foi corretamente configurado"
-task :before_symlink do
-  on_rollback {}
-  run "test -d #{release_path}/tmp || mkdir -m 755 #{release_path}/tmp"
-  run "test -d #{release_path}/db || mkdir -m 755 #{release_path}/db"
-  run "cp #{deploy_to}/etc/database.yml #{release_path}/config/database.yml"
-  run "cd #{release_path} && rake db:migrate RAILS_ENV=production"
-end
 
-desc "Garante que as configuracoes estao adequadas"
+desc "send config files to server"
 task :before_setup do
-  ts = Time.now.strftime("%y%m%d%H%M%S")
   
+  run "test -d #{deploy_to}/etc || mkdir -m 755 #{deploy_to}/etc"
   upload File.join(File.dirname(__FILE__), "templates/database.yml"), "#{deploy_to}/etc/database.yml"
   upload File.join(File.dirname(__FILE__), "templates/backup.rb"), "#{deploy_to}/etc/backup.rb"
   upload File.join(File.dirname(__FILE__), "templates/ssh_helper.rb"), "#{deploy_to}/etc/ssh_helper.rb"
   
 end
 
-desc "Prepare the production database before migration"
-task :before_cold do
-end
-
 namespace :deploy do
-  desc "Pede restart ao servidor Passenger"
+  desc "Restart with Passenger"
   task :restart, :roles => :app do
     run "touch #{deploy_to}/current/tmp/restart.txt"
   end
-end
+ 
+  desc "Setup a GitHub-style deployment."
+  task :setup, :except => { :no_release => true } do
+    run "git clone #{repository} #{current_path}"
+    run "test -d #{current_path}/tmp || mkdir -m 755 #{current_path}/tmp"
+    run "test -d #{current_path}/db || mkdir -m 755 #{current_path}/db"
+  end
+ 
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+  end
+ 
+  desc "check if the configurations files are present"
+  task :symlink do
+    on_rollback {}
+    run <<-CMD
+      rm -rf #{current_path}/log #{current_path}/public/system #{current_path}/tmp/pids #{current_path}/config/database.yml &&
+      ln -s #{shared_path}/log #{current_path}/log &&
+      ln -s #{shared_path}/system #{current_path}/public/system &&
+      ln -s #{shared_path}/pids #{current_path}/tmp/pids &&
+      ln -s #{deploy_to}/etc/database.yml #{current_path}/config/database.yml &&
+      cd #{current_path} && rake db:migrate RAILS_ENV=production
+    CMD
+  end
 
-[:start, :stop].each do |t|
-    desc "A tarefa #{t} nao eh necessaria num ambiente com Passenger"
-    task t, :roles => :app do ; end
+  namespace :rollback do
+    task :default do
+      revision
+      restart
+    end
+    
+    desc "Rollback a single commit."
+    task :revision, :except => { :no_release => true } do
+      set :branch, "HEAD^"
+      default
+    end
+  end
+
 end
 
 namespace :log do
@@ -103,5 +121,3 @@ namespace :db do
     run "if [ -f #{backup_rb} ]; then ruby #{backup_rb} drop_all #{deploy_to} ; fi"
   end
 end
-
-after "deploy:update", "deploy:cleanup"
