f89a097538379f080b93c83d533d83ef28196149
Marcio Trindade <marciotrindade@gmail.com>
2009-09-21 04:07:33 -0300

    move data_table to helpers with a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,19 @@ module ApplicationHelper
 	def stylesheet(*files)
 		content_for(:head) { stylesheet_link_tag(*files) }
 	end
+
+  def data_table(options={})
+   options.reverse_merge! "sPaginationType" => "full_numbers",
+                          "bStateSave" => true,
+                          "bInfo" => false,
+                          "iDisplayLength" => "100",
+                          "aaSorting" => [[ 0, "asc"]],
+                          "oLanguage" => {"sUrl" => "/javascripts/plugins/data_tables_#{I18n.locale}.js"}
+   content_for(:head) do
+     javascript_tag do
+       "$(function(){oTable = $('#table').dataTable(#{options.to_json}).css('width', 'auto');});"
+     end
+   end
+  end
+
 end
