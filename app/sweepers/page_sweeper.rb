edd86c505d50f37f3b1dcdbfb3ec4d5d1513584d
Philipe Casarotte <phisoccer@globo.com>
2009-10-17 20:13:04 -0300

    sweeper pages

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -5,6 +5,9 @@ class PageSweeper < ApplicationSweeper
   def after_clear_cache(model)
     expire_page('/index')
     sweep_directory(index_model_path(model), "/", "system", "images", "javascripts")
+    sweep_directory("blog")
+    sweep_directory("fotos")
+    sweep_directory("downloads")
   end
 
 end
